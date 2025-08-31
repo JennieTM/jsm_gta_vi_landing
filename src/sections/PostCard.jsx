fda37483f5a438cfdba6deb2fc62ab739fd4eb88
JennieTM <ibaranov445@gmail.com>
2025-08-31 15:42:33 +0300

    Update PostCard.jsx

--- src/sections/PostCard.jsx
+++ src/sections/PostCard.jsx
@@ -25,7 +25,7 @@ const PostCard = () => {
       <div className="animated-gradient-bg" />
 
       <div className="post-card-wrapper group hover:rotate-1 hover:-[1.02] transition duration-700">
-        <img src="/images/overlay.webp" />
+        <img src="" />
 
         <video 
           ref={videoRef}
